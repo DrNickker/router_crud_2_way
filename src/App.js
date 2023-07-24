2685419a4933222f094f4b02387a7234edead88b
DrNickker <mintagirov2015@yandex.ru>
2023-07-24 22:00:15 +0500

    add_files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// modules
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// components
+import PostList from "./components/PostList/PostList";
+import NewPost from "./components/NewPost/NewPost";
+import getRequest from "./requests/getRequest.js";
 
 function App() {
+  const [posts, setPosts] = useState(null);
+  const [isloading, setloading] = useState(false);
+
+  useEffect(() => {
+    console.log('useEffect app js');
+    const newData = getRequest(process.env.REACT_APP_POSTS_URL, "posts", setloading);
+    newData.then(data => setPosts(data));
+  }, []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="notice">
+        для работы приложения требуется паралельный активный роутер{" "}
+        <strong>запусти в терминале "cd /backend/ && npm start"</strong>
+      </p>
+      <hr />
+      <Router>
+        <div className="page">
+          <Routes>
+            <Route path="/" element={<PostList posts={posts} loading={isloading} />} />
+            <Route path="/posts/new" element={<NewPost />} />
+            <Route path="/posts/:id" element={<div>post</div>} />
+            <Route path="/pists/edit/:id" element={<div>edit post</div>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
